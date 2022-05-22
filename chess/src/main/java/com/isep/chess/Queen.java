ee100130b0196cc69c997aa55756c3a8768b57de
glachaud <guillaume.lachaud78@gmail.com>
2022-05-22 17:42:03 +0200

    Adds Queen class.
    Implements the Queen class.
    All the heavy work has already been done in one way or another in the
    Piece class (for the linear and diagonal moves) and the Bishop and Rook
    classes (for applying the Piece methods).

---
+++ chess/src/main/java/com/isep/chess/Queen.java
@@ -0,0 +1,28 @@
+package com.isep.chess;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Queen extends Piece {
+  public Queen(int color, Square currentSquare) {
+    super(color, currentSquare);
+  }
+
+  @Override
+  public List<Square> getLegalPositions(Board b) {
+    List<Square> legalPositions = new LinkedList<>();
+    Square[][] board = b.getBoardArray();
+    int row = this.getPosition().getRow();
+    int col = this.getPosition().getCol();
+
+    int[] positionLimits = getLinearOccupations(board, row, col);
+    for (int i = positionLimits[0]; i <= positionLimits[1]; i++) {
+      if (i != row) legalPositions.add(board[i][col]);
+    }
+    for (int j = positionLimits[2]; j <= positionLimits[3]; j++) {
+      if (j != col) legalPositions.add(board[row][j]);
+    }
+    legalPositions.addAll(getDiagonalOccupations(board, row, col));
+    return legalPositions;
+  }
+}
