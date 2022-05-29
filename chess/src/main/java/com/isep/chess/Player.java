a6e2a8125847fc542b60bc2c4589bfed3e622fb1
glachaud <guillaume.lachaud78@gmail.com>
2022-05-29 16:24:24 +0200

    Adds inCheck method to Player class.
    Implements inCheck method that determines whether a Player is in check
    or not, depending on the opponent's available Moves.

--- chess/src/main/java/com/isep/chess/Player.java
+++ chess/src/main/java/com/isep/chess/Player.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class Player {
   private final int color;
   private Board b;
+  private List<Square> squares;
   private List<Piece> alivePieces;
   private King king;
   private Map<Square, List<Piece>> availableMoves;
@@ -19,26 +20,43 @@ public class Player {
     this.movableSquares = new LinkedList<>();
     this.b = b;
     Square[][] board = b.getBoardArray();
-    for (int i = 0; i < Board.BOARD_SIZE; i++) {
-      for (int j = 0; j < Board.BOARD_SIZE; j++) {
-        this.availableMoves.put(board[i][j], new LinkedList<>());
+    for (int row = 0; row < Board.BOARD_SIZE; row++) {
+      for (int column = 0; column < Board.BOARD_SIZE; column++) {
+        squares.add(board[row][column]);
+        this.availableMoves.put(board[row][column], new LinkedList<>());
       }
     }
   }
 
   public void update() {
     // Empty the list of availableMoves at each turn
-    for (List<Piece> pieces : availableMoves.values()){
+    for (List<Piece> pieces : availableMoves.values()) {
       pieces.removeAll(pieces);
     }
     this.movableSquares.removeAll(movableSquares);
     // Add all the availableMoves (except for the King's)
     alivePieces.removeIf((piece -> !piece.getClass().equals(King.class) && piece.getPosition() == null));
-    for(Piece p: alivePieces){
-      for(Square legalMoves : p.getLegalPositions(b)){
+    for (Piece p : alivePieces) {
+      for (Square legalMoves : p.getLegalPositions(b)) {
         this.availableMoves.get(legalMoves).add(p);
       }
     }
+  }
+
+  /**
+   * Determines whether the king is in check.
+   * If the king is not in check, all the squares are movable, provided that the move does not lead to
+   * a check position to the player.
+   * @param opponentAvailableMoves {@link Map} of {@link Square} indicating which opponent's piece can reach square.
+   * @return {@code true} if the king is in check, {@code false} otherwise.
+   */
+  public boolean inCheck(Map<Square, List<Piece>> opponentAvailableMoves) {
+    Square kingPosition = king.getPosition();
+    if (opponentAvailableMoves.get(kingPosition).isEmpty()) {
+      movableSquares.addAll(squares);
+      return false;
+    }
+    return true;
   }
 
   public int getColor() {
