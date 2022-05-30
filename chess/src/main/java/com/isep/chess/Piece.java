a22c2c9a457f31c154c1d2b0ac70293df5a35b9e
glachaud <guillaume.lachaud78@gmail.com>
2022-05-30 11:15:53 +0200

    Adds linear and diagonal block methods in Piece.
    Implements getLinearBlocks in the Piece class that find the horizontal
    or vertical squares that can be used to block a check.
    Implements getDiagonalBlocks in the Piece class that find the diagonal squares
    that can be used to block a check.

--- chess/src/main/java/com/isep/chess/Piece.java
+++ chess/src/main/java/com/isep/chess/Piece.java
@@ -1,5 +1,6 @@
 package com.isep.chess;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -128,7 +129,80 @@ public abstract class Piece {
 
   }
 
+  /**
+   * Returns the list of {@link Square} that stand between the piece and its prey in
+   * either a horizontal or vertical way.
+   * It is important for finding a way to block a check.
+   * @param b represents the squares of the chess board, which can be occupied or empty.
+   * @param p represents the piece to be captured, or checked if it is the king.
+   * @return a list of {@link Square} that can block the capture.
+   */
+  public List<Square> getLinearBlocks(Board b, Piece p) {
+    List<Square> blockableSquares = new LinkedList<>();
+    int row = this.getPosition().getRow();
+    int col = this.getPosition().getCol();
+    int prey_row = this.getPosition().getRow();
+    int prey_col = this.getPosition().getCol();
+    Square[][] board = b.getBoardArray();
+
+    if (row == prey_row) {
+      int min_col = Math.min(col, prey_col);
+      int max_col = Math.max(col, prey_col);
+      blockableSquares.addAll(Arrays.asList(board[row]).subList(min_col + 1, max_col));
+    }
+    if (col == prey_col) {
+      int min_row = Math.min(row, prey_row);
+      int max_row = Math.max(row, prey_row);
+      for (int i = min_row + 1; i < max_row; i++)
+        blockableSquares.add(board[i][col]);
+    }
+    return blockableSquares;
+  }
+
+  /**
+   * Returns the list of {@link Square} that stand between the piece and its prey in
+   * a diagonal way.
+   * It is important for finding a way to block a check. The four for loops describe
+   * either of the relative positions between the two pieces: upper right diagonal,
+   * lower left diagonal, etc.
+   * @param b represents the squares of the chess board, which can be occupied or empty.
+   * @param p represents the piece to be captured, or checked if it is the king.
+   * @return a list of {@link Square} that can block the capture.
+   */
+  public List<Square> getDiagonalBlocks(Board b, Piece p) {
+    List<Square> blockableSquares = new LinkedList<>();
+    int row = this.getPosition().getRow();
+    int col = this.getPosition().getCol();
+    int prey_row = this.getPosition().getRow();
+    int prey_col = this.getPosition().getCol();
+    Square[][] board = b.getBoardArray();
+
+    if (row > prey_row && col > prey_col) {
+      for (int i = row + 1, j = col + 1; i < prey_row && j < prey_col; i++, j++) {
+        blockableSquares.add(board[i][j]);
+      }
+    }
+    if (row > prey_row && col < prey_col) {
+      for (int i = row + 1, j = col - 1; i < prey_row && j > prey_col; i++, j--) {
+        blockableSquares.add(board[i][j]);
+      }
+    }
+    if (row < prey_row && col > prey_col) {
+      for (int i = row - 1, j = col + 1; i > prey_row && j < prey_col; i--, j++) {
+        blockableSquares.add(board[i][j]);
+      }
+    }
+    if (row < prey_row && col < prey_col) {
+      for (int i = row - 1, j = col - 1; i > prey_row && j > prey_col; i--, j--) {
+        blockableSquares.add(board[i][j]);
+      }
+    }
+    return blockableSquares;
+  }
+
   public abstract List<Square> getLegalPositions(Board b);
+
+  abstract List<Square> getBlockableSquares(Board b, Piece prey);
 
   public void setPosition(Square square) {
     this.currentSquare = square;
